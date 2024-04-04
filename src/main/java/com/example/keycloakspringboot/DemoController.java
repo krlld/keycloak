7885f3f92f71bee56b384e8c97d3853490d7fe0d
krlld <kirilldikun25@gmail.com>
2024-04-04 19:23:37 +0300

    feat: configure integration between keycloak and spring-boot application

---
+++ src/main/java/com/example/keycloakspringboot/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.keycloakspringboot;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @PreAuthorize("hasRole('CLIENT_USER')")
+    @GetMapping("/user")
+    public String userResource() {
+        return "Some user info";
+    }
+
+    @PreAuthorize("hasRole('CLIENT_ADMIN')")
+    @GetMapping("/admin")
+    public String adminResource() {
+        return "Some admin info";
+    }
+}
