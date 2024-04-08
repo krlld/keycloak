a7ec82735fe076be86dcf4c147154b39d86953be
krlld <kirilldikun25@gmail.com>
2024-04-08 14:08:21 +0300

    feat: add repository service and filter request interfaces

---
+++ src/main/java/com/example/keycloakspringboot/repository/RepositoryService.java
@@ -0,0 +1,23 @@
+package com.example.keycloakspringboot.repository;
+
+import com.example.keycloakspringboot.dto.FilterRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RepositoryService<E, I> {
+
+    Optional<E> create(E entity);
+
+    Optional<E> find(I id);
+
+    Optional<E> update(E entity);
+
+    void delete(I id);
+
+    List<E> findList(FilterRequest filter);
+
+    Page<E> findPage(FilterRequest filter, Pageable pageable);
+}
