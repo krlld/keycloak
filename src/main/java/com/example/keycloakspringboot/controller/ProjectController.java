8faceb553ba4cd2b89dd4272963ea311cd805166
krlld <kirilldikun25@gmail.com>
2024-04-09 12:49:08 +0300

    refactor: add lombok

--- src/main/java/com/example/keycloakspringboot/controller/ProjectController.java
+++ src/main/java/com/example/keycloakspringboot/controller/ProjectController.java
@@ -3,7 +3,7 @@ package com.example.keycloakspringboot.controller;
 import com.example.keycloakspringboot.dto.ProjectDto;
 import com.example.keycloakspringboot.dto.ProjectFilterDto;
 import com.example.keycloakspringboot.service.ServiceInterface;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +16,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/projects")
+@RequiredArgsConstructor
 public class ProjectController {
 
     private final ServiceInterface<ProjectDto, Long> serviceInterface;
-
-    public ProjectController(ServiceInterface<ProjectDto, Long> serviceInterface) {
-        this.serviceInterface = serviceInterface;
-    }
 
     @PreAuthorize("hasAnyRole('hr')")
     @GetMapping
