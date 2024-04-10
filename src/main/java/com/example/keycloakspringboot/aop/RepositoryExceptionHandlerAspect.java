58d4eb195a9a6bbab0595980c2a02361c5adc207
MaksGavriliuk <maks.revival27@gmail.com>
2024-04-10 12:49:15 +0300

    refactor: add @Component to RepositoryExceptionHandlerAspect

--- src/main/java/com/example/keycloakspringboot/aop/RepositoryExceptionHandlerAspect.java
+++ src/main/java/com/example/keycloakspringboot/aop/RepositoryExceptionHandlerAspect.java
@@ -4,17 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
+@Component
 public class RepositoryExceptionHandlerAspect {
 
     @Around("execution(* com.example.keycloakspringboot.service.ServiceInterface.*(..))")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
-            log.info("Сработало AOP");
             return joinPoint.proceed();
         } catch (Exception e) {
+            e.getMessage();
             e.printStackTrace();
             return null;
         }
