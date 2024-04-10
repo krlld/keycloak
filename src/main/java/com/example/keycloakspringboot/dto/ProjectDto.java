2bb93e2ebdf3e87df02698f2d2723870f590dfed
krlld <kirilldikun25@gmail.com>
2024-04-10 13:15:55 +0300

    feat: add validation annotations

--- src/main/java/com/example/keycloakspringboot/dto/ProjectDto.java
+++ src/main/java/com/example/keycloakspringboot/dto/ProjectDto.java
@@ -1,6 +1,9 @@
 package com.example.keycloakspringboot.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -12,15 +15,21 @@ public class ProjectDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
+    @NotBlank
     private String title;
 
+    @NotBlank
     private String description;
 
+    @Min(0)
     private BigDecimal budget;
 
+    @NotNull
     private LocalDate startAt;
 
+    @NotNull
     private LocalDate endAt;
 
+    @NotBlank
     private String status;
 }
